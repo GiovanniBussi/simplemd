6cffb370bc04c5a64a935a832269fec97f7de716
Giovanni Bussi <giovanni.bussi@gmail.com>
2024-06-18 17:10:43 +0200

    added tool to generate positions

--- python/simplemd.py
+++ python/simplemd.py
@@ -159,6 +159,21 @@ def _compute_list(cell,positions,listcutoff,nlist,point):
                 point[i+1]+=1
 
 
+def generate_lattice(n=1, a0=1.6796):
+    coord=np.zeros((4*n*n*n,3))
+    l=0
+    for i in range(n):
+       for j in range(n):
+           for k in range(n):
+               coord[l]=(i,j,k)
+               l+=1
+               coord[l]=(i+0.5,j,k+0.5)
+               l+=1
+               coord[l]=(i+0.5,j+0.5,k)
+               l+=1
+               coord[l]=(i,j+0.5,k+0.5)
+               l+=1
+    return ((n*a0,n*a0,n*a0),coord*a0)
 class SimpleMD:
     def __init__(self):
         self.iv=np.zeros(32,dtype=int)
